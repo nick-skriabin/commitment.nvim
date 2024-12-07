300e92667fa5009a4bbff801fd9abcf8da663969
Nick Skriabin <nr@fenelon.ru>
2024-12-07 18:25:07 +0000

    Adding help file

--- lua/commitment/init.lua
+++ lua/commitment/init.lua
@@ -11,27 +11,27 @@
 --- ## Installation:
 ---
 --- ### Lazy
---- ```lua
+--- @usage >lua
 --- {
 ---   "whaledev/commitment.nvim",
 ---   opts = {}
 --- }
---- ```
+--- <
 ---
 --- ### Packer
---- ```lua
+--- @usage >lua
 --- use {
 ---   "whaledev/commitment.nvim",
 ---   config = function()
 ---     require("commitment").setup()
 ---   end,
 --- }
---- ```
+--- <
 ---
 --- ### Vim-Plug
---- ```vim
+--- @usage >vim
 --- Plug 'whaledev/commitment.nvim'
---- ```
+--- <
 ---
 --- ### Default config
 --- ```lua
@@ -56,27 +56,29 @@ local utils = require("commitment.utils")
 local git = require("commitment.git")
 
 local default_opts = {
-    -- Regular message. Shown when writes limit is reached or timer fired.
+    --- Regular message. Shown when writes limit is reached or timer fired.
     message = "Don't forget to git commit!",
-    -- Message shown when writes are prevented.
+    --- Message shown when writes are prevented.
     message_write_prevent = "You shall not write!",
-    -- Message shown when useless commit message is detected.
+    --- Message shown when useless commit message is detected.
     message_useless_commit = "That's not a very useful commit message, mind rephrasing it?",
-    -- Prevents writes to file until changes are committed.
+    --- Prevents writes to file until changes are committed.
     stop_on_write = false,
-    -- Prevent writes to file when useless commit message is detected.
+    --- Prevent writes to file when useless commit message is detected.
     stop_on_useless_commit = false,
-    -- Number of writes before asking to commit.
+    --- Number of writes before asking to commit.
     writes_number = 30,
-    -- Interval in minutes to check git tree for changes.
+    --- Interval in minutes to check git tree for changes.
     check_interval = -1,
 }
 
-local writes_count = 0
+WRITES_COUNT = 0
 local locked = false
 
 -- Module start
-local M = {}
+local M = {
+    config = default_opts,
+}
 
 --- Handles writing to file
 --- Will prevent writes to file if `locked` is true
@@ -166,11 +168,7 @@ end
 --- Sets up an autocmd to prevent writing to the file
 --- when `opts.prevent_write` is true.
 ---
----@param opts table Module config table.
----
-local function setup_write_prevent_autocmd(opts)
-    local notify = notifier().notify
-
+function M.setup_write_prevent_autocmd(self)
     utils.autocmd({ "BufWriteCmd" }, {
         group = utils.autogroup("commitment-preven", true),
         callback = custom_write,
@@ -179,10 +177,10 @@ end
 
 --- Gets the message to be displayed to the user
 ---
----@param opts table Module config table.
 ---@param alt boolean? Indicates that an alternative message should be used.
 ---
-local function get_message(opts, alt)
+function M.get_message(self, alt)
+    local opts = self.config
     local extra_message = opts.prevent_write and locked and "\n(writing to file disabled)" or ""
     local main_message = opts.prevent_write and opts.message_write_prevent or opts.message
     if alt then
@@ -196,46 +194,42 @@ end
 --- or if the commit message is useless. It will also disable writing
 --- to the file when `opts.prevent_write` is true.
 ---
----@param opts table Module config table.
----
-local function setup_watcher_autocmd(opts)
+function M.setup_watcher_autocmd(self)
     local n = notifier()
     utils.autocmd({ "BufWritePre" }, {
         group = utils.autogroup("commitment-watch", true),
         callback = function()
             local clean = git.git_tree_is_clean()
-            local exceeded_writes = writes_count > opts.writes_number
+            local exceeded_writes = WRITES_COUNT > self.config.writes_number
             local useless = git.is_useless_commit()
 
             if clean and not useless then
                 locked = false
-                write_count = 0
+                WRITE_COUNT = 0
             elseif (not clean and exceeded_writes) or (clean and useless) then
                 locked = true
-                n.notify(get_message(opts, useless))
+                n.notify(self:get_message(useless))
             end
-            writes_count = writes_count + 1
+            WRITES_COUNT = WRITES_COUNT + 1
         end,
     })
 end
 
 --- Runs the watcher with `opts.check_interval` interval in minutes
 ---
----@param opts table Module config table.
----
-local function run_scheduled(opts)
+function M.run_scheduled(self)
     local n = notifier()
     vim.defer_fn(function()
         local clean = git.git_tree_is_clean()
         local useless = git.is_useless_commit()
         if not clean or useless then
-            n.notify(get_message(opts, useless))
+            n.notify(self:get_message(useless))
             locked = true
         else
             locked = false
         end
-        run_scheduled(opts)
-    end, opts.check_interval * 60 * 1000)
+        self:run_scheduled()
+    end, self.config.check_interval * 60 * 1000)
 end
 
 --- Module setup
@@ -247,21 +241,21 @@ end
 ---   -- OR
 ---   require('commitment').setup({}) -- replace {} with your config table
 --- <
-function M.setup(config)
-    config = utils.deep_merge_opts(config)
+function M.setup(self, config)
     if not git.is_git_repo() then
         return
     end
+    self.config = utils.deep_merge_opts(config or {}, self.config)
 
-    if config.prevent_write then
-        setup_write_prevent_autocmd(config)
+    if self.config.prevent_write then
+        self:setup_write_prevent_autocmd()
     end
 
-    if config.check_interval == -1 then
-        setup_watcher_autocmd(config)
+    if self.config.check_interval == -1 then
+        self.setup_watcher_autocmd()
         return
     else
-        run_scheduled(config)
+        M:run_scheduled()
     end
 end
 
--- lua/commitment/utils.lua
+++ lua/commitment/utils.lua
@@ -1,19 +1,33 @@
 local M = {}
 
+--- Creates an augroup
+---
+---@param name string The name of the augroup.
+---@param clear boolean? Whether to clear the group before creating it.
+---
 M.autogroup = function(name, clear)
     local clear_group = true
 
     if clear == nil then
-        clear_group = clear
+        clear_group = clear == nil and true or clear
     end
 
     return vim.api.nvim_create_augroup(name, { clear = clear_group })
 end
 
+--- Creates an autocmd
+---
+---@param events string|string[] The events to be listened to.
+---@param options table The options for the autocmd.
+---
 M.autocmd = function(events, options)
     vim.api.nvim_create_autocmd(events, options)
 end
 
+--- Notifies the user with a message
+---
+---@param msg string The message to be displayed.
+---
 function M.notify(msg)
     -- if snacks.nvim is available, use it
     if pcall(require, "snacks") then
@@ -25,6 +39,30 @@ function M.notify(msg)
         return
     end
     vim.notify(msg, "warn")
+end
+
+--- Merges two tables recursively
+---
+---@param table1 table
+---@param table2 table
+---
+function M.deep_merge_opts(table1, table2)
+    local default_opts = table2
+    local opts = table1
+
+    for k, v in pairs(default_opts) do
+        if type(v) == "table" then
+            if opts[k] == nil then
+                opts[k] = {}
+            end
+            opts[k] = M.deep_merge_opts(opts[k], v)
+        else
+            if opts[k] == nil then
+                opts[k] = v
+            end
+        end
+    end
+    return opts
 end
 
 return M
